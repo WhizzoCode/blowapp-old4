6a16d547255f0af1a729cdb68223fe59e7ffb532
Sergio Martín <sergio@sergiomartin.dev>
2022-10-22 10:37:29 +0200

    Add layout

--- src/lib/BlowApp.js
+++ src/lib/BlowApp.js
@@ -1,4 +1,4 @@
-import {LitElement, html} from 'lit';
+import {LitElement, css, html} from 'lit';
 
 export class BlowApp extends LitElement {
 
@@ -6,9 +6,31 @@ export class BlowApp extends LitElement {
     super();
   }
 
+  static styles = css`
+    sl-split-panel,
+    sl-split-panel > div {
+      height: 100%;
+    }
+
+    sl-split-panel > div:nth-child(1) {
+      background: var(--sl-color-neutral-50);
+    }
+
+    sl-split-panel > div:nth-child(2) {
+      background: var(--sl-color-neutral-0);
+    }
+  `;
+
   render() {
     return html`
-      <h1>BlowApp</h1>
+      <sl-split-panel position-in-pixels="300" snap="300px">
+        <div slot="start">
+
+        </div>
+        <div slot="end">
+
+        </div>
+      </sl-split-panel>
     `;
   }
 
--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
-import { setBasePath } from '@shoelace-style/shoelace/dist/utilities/base-path';
-import { BlowApp } from './lib/BlowApp.js';
-
-import './source-sans-3VF.css';
 import '@shoelace-style/shoelace/dist/themes/dark.css';
+import './source-sans-3VF.css';
 import './style.css';
+
+import { setBasePath } from '@shoelace-style/shoelace/dist/utilities/base-path';
+import '@shoelace-style/shoelace/dist/components/split-panel/split-panel.js';
+import { BlowApp } from './lib/BlowApp.js';
 
 setBasePath('shoelace');
 
