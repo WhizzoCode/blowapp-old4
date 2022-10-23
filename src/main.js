2af2e74348940186c044e30b766e5224ed0c935d
Sergio Martín <sergio@sergiomartin.dev>
2022-10-24 01:23:34 +0200

    Add gallery-panel element

--- src/main.js
+++ src/main.js
@@ -8,6 +8,7 @@ import '@shoelace-style/shoelace/dist/components/tree/tree.js';
 import '@shoelace-style/shoelace/dist/components/tree-item/tree-item.js';
 
 import './lib/BlowApp.js';
+import './lib/GalleryPanel';
 
 setBasePath('shoelace');
 
